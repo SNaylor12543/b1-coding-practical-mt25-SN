9c8bea513495ec5feabeeda62d6e369bcbaa617d
SNaylor12543 <sam@grahamnaylor.net>
2025-10-17 13:24:30 +0100

    Controller now able to handle T=0

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -2,21 +2,22 @@ import numpy as np
 
 class Controller:
     def __init__(self):
+        # Defining Proportional and Differential Gain values
         self.Kp = 0.15
         self.Kd = 0.6
     
     def controller(self, Reference: np.ndarray, Position: np.ndarray, T: int):
         # Reference and Position should be the full arrays, and T should be the time starting from 0
 
-        # Defining Proportional and Differential Gain values
-        #Kp = 0.15
-        #Kd = 0.6
-
         # Creating Error array
         Error = np.subtract(Reference, Position)
 
-        # u(t) = Kp * e(t) + Kd * (e(t) - e(t-1))
-        System_Input = self.Kp * Error[T] + self.Kd * (Error[T] - Error[T-1])
+        if T==0: # If the submarine has just started there can be no derivative term
+            System_Input = self.Kp * Error[T]
+            
+        else:    # Otherwise use proportional and differential terms
+            # u(t) = Kp * e(t) + Kd * (e(t) - e(t-1))
+            System_Input = self.Kp * Error[T] + self.Kd * (Error[T] - Error[T-1])
 
         return System_Input
     
